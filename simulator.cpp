8f8abeb36268101158a9759812a8615d68fa39a6
therazix <therazix@gmail.com>
2021-12-09 18:06:10 +0100

    Connected GUI to simulator, minor changes

--- simulator.cpp
+++ simulator.cpp
@@ -1,7 +1,12 @@
 #include "simulator.h"
 using namespace std;
 
-int main() {
+int main()
+{
+    return open_window(simulator_main, AREA_SIZE);
+}
+
+int simulator_main() {
     std::srand(std::time(0));
     std::vector<std::vector<int>> area( AREA_SIZE , vector<int> ( AREA_SIZE ));
     std::vector<Person*> people;
@@ -51,18 +56,45 @@ int main() {
     //     cout << endl;
     // }
 
+    // Run simulation
+    for (int i = 0; i < SIMULATED_CYCLES; i++)
+    {
+        // Refresh UI
+        if (i % REFRESH_INTERVAL == 0)
+        {
+            refresh_window(&people);
+        }
+
+        // Test - start moving every person to middle
+        for (int i = 0; i < people.size(); i++)
+        {
+            if (people[i]->currentX < AREA_SIZE / 2)
+                (people[i]->currentX)++;
+            else
+                (people[i]->currentX)--;
+
+            if (people[i]->currentY < AREA_SIZE / 2)
+                (people[i]->currentY)++;
+            else
+                (people[i]->currentY)--;
+        }
+    }
+
     return 1;
 }
 
 void generatePeople(AGE_GROUP ageGroup, std::vector<Person*>* people) {
     if ((START_AGE_GROUP_CNT[ageGroup] / SCALE) == 0) return;
 
-    for (int j = 0; j <= (START_AGE_GROUP_CNT[ageGroup] / SCALE); j++) {
+    for (int j = 0; j < (START_AGE_GROUP_CNT[ageGroup] / SCALE); j++) {
         Person* newPerson = new Person();
         newPerson->ageGroup = ageGroup;
+        newPerson->infectionState = NOT_INFECTED;
         newPerson->vaccinationState = NOT_VACCINATED; // No one is vaccinated from start
         newPerson->homeX = newPerson->currentX = rand() % (AREA_SIZE);
         newPerson->homeY = newPerson->currentY = rand() % (AREA_SIZE);
+        newPerson->currentX = newPerson->homeX;
+        newPerson->currentY = newPerson->homeY;
         (*people).push_back(newPerson);
     }
 }
--- simulator.h
+++ simulator.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "person.h"
+#include "app_window.h"
 #include <iostream>
 #include <vector>
 #include <array>
 #include <ctime>
+
+// How many cycles the simulation should run
+#define SIMULATED_CYCLES 5000
+
+// After how many cycles the GUI should refresh (higher value means faster animation)
+#define REFRESH_INTERVAL 2
 
 int SCALE = 100; 
 int AREA_SIZE_NOT_SCALED = 496000; // Area of Prague in meters
@@ -11,5 +18,9 @@ int START_INFECTED_CNT_NOT_SCALED = 32413; // Number of infected people on 6.12.
 
 int AREA_SIZE = AREA_SIZE_NOT_SCALED/SCALE;
 int START_INFECTED_CNT = START_INFECTED_CNT_NOT_SCALED/SCALE;
+
+int main();
+
+int simulator_main();
 
 void generatePeople(AGE_GROUP ageGroup, std::vector<Person*>* people);
